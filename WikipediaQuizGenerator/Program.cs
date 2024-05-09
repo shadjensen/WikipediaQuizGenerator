18beef19a30fe8c591a6cd2e09cbf626d4449df8
u1287538 <u1287538@umail.utah.edu>
2024-05-09 16:06:47 -0600

    Added a host and join screen to the project

--- WikipediaQuizGenerator/Program.cs
+++ WikipediaQuizGenerator/Program.cs
@@ -29,7 +29,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseAntiforgery();
 
-app.MapHub<PlayerHub>("/chathub");
+app.MapHub<PlayerHub>("/playerhub");
 
 app.MapRazorComponents<App>()
     .AddInteractiveWebAssemblyRenderMode()
