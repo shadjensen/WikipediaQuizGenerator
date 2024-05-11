9d6335fd326f3d8eddbe6131ea42db7ccb23dac8
u1287538 <u1287538@umail.utah.edu>
2024-05-10 19:31:45 -0600

    Fixed server functionality. Now sends and recieves correct protocols

--- WikipediaQuizGenerator/Hubs/PlayerHub.cs
+++ WikipediaQuizGenerator/Hubs/PlayerHub.cs
@@ -1,14 +1,42 @@
 ﻿
 using Microsoft.AspNetCore.SignalR;
+using WikipediaQuizGenerator.Services;
 
 namespace WikipediaQuizGenerator.Hubs
 {
+    
     public class PlayerHub : Hub
     {
+        private readonly IServiceProvider serviceProvider;
+        public PlayerHub(IServiceProvider _serviceProvider) 
+        {
+            serviceProvider = _serviceProvider;
+        }
         public async Task SendMessage(string user, string message) 
         {
             await Clients.All.SendAsync("RecieveMessage", user, message);
         
         }
+
+        public async Task JoinLobby(string username, string userCountry) 
+        {
+            PlayerDataServices? playerDataServices = serviceProvider.GetService<PlayerDataServices>();
+        
+            if (playerDataServices != null) 
+            {
+                //we add the player to our three libraries, and set their initial score to zero
+                playerDataServices.Players.Add(username);
+                playerDataServices.PlayerCountries.Add(username, userCountry);
+                playerDataServices.PlayerScores.Add(username, 0);
+
+                await Clients.Caller.SendAsync("LobbyJoined", username);
+            }
+            else
+                await Clients.Caller.SendAsync("LobbyJoined", "Could not resolve playerDataServices");
+        }
+
+        
+
+
     }
 }
